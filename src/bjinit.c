0b41bc7aed39de3dcf4d314a3f15906defd5ac83
gtheler <jeremy@seamplex.com>
2019-08-20 06:17:55 -0300

    missing stdlib

--- src/bjinit.c
+++ src/bjinit.c
@@ -19,6 +19,7 @@
  *  along with libreblackjack.  If not, see <http://www.gnu.org/licenses/>.
  *------------------- ------------  ----    --------  --     -       -         -
  */
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
