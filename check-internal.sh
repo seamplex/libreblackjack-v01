4e698607829ed938c6a5858ec784af50d67995aa
gtheler <jeremy@seamplex.com>
2019-08-21 17:52:24 -0300

    checks as standalone scripts

---
+++ check-internal.sh
@@ -0,0 +1,6 @@
+if [ ! -z "`which awk`" ]; then
+  ./libreblackjack -i 2>&1 | grep return | awk '{e=-4e-3;s=1e-2} END {ok = ($2>(e-s)&&$2<(e+s)); print ok?"ok":"failed"; exit !ok }'
+  result=$?
+else
+  result=77
+fi
