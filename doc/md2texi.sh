4eed56625da0f40aa86b1605f1872576c4dd61e4
jeremy theler <jeremy@seamplex.com>
2020-02-23 10:44:13 -0300

    m4 -> md -> texi

--- doc/md2texi.sh
+++ doc/md2texi.sh
@@ -2,4 +2,4 @@ if [ -z "`which pandoc`" ]; then
  echo "error: pandoc not installed"
  exit 1
 fi
-pandoc blackjack.md --toc --template template.texi -o blackjack.texi
+m4 header.m4 blackjack.md | pandoc --toc --template template.texi -o blackjack.texi
